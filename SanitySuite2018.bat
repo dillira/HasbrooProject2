df89a7dac0c1376bc1c0f4e1c772517753bb2b01
dilli <dilli.r.kumaran.e.l@accenture.com>
2018-08-30 15:22:34 +0530

    Commit5

--- SanitySuite2018.bat
+++ SanitySuite2018.bat
@@ -1,6 +1,6 @@
 @echo "Starting Selenium test1"
 
-set projectLocation=C:\Users\dilli.r.kumaran.e.l\Automation\GitPull\Hasbro15Feb\Hasbro\FlexPLM
+set projectLocation=C:\Hasbro15Feb\Hasbro\FlexPLM
 cd %projectLocation%
 set classpath=%projectLocation%\bin;%projectLocation%\lib\*
 java org.testng.TestNG %projectLocation%\SanitySuite2018.xml
